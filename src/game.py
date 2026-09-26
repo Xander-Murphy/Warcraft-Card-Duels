c371318acc16cd828ced4332c4fb643393080dbf
Xander <xanderjm1116@gmail.com>
2026-09-26 13:46:31 -0400

    dungeon selection screen and placeholder dungeon screen have been added

--- src/game.py
+++ src/game.py
@@ -20,6 +20,15 @@ class Game:
     self.selected_heroes = [] # stores heroes the player selects for their team
     self.hero_selection = 0 # starts the selected hero list at the first entry
 
+    self.available_dungeons = [
+      "Ragefire Chasm",
+      "Wailing Caverns",
+      "The Deadmines",
+    ]
+
+    self.dungeon_selection = 0
+    self.selected_dungeon = None
+
   def handle_events(self): # This function handles events checks the current game state and uses specific screen functions depending on the current state
     for event in pygame.event.get():
       if event.type == pygame.QUIT: 
@@ -32,6 +41,12 @@ class Game:
 
         elif self.state == GameState.TEAM_SELECTION: 
           self.handle_team_selection_input(event)    
+
+        elif self.state == GameState.DUNGEON_SELECTION:
+          self.handle_dungeon_selection_input(event)
+
+        elif self.state == GameState.ENCOUNTER:
+          self.handle_encounter_input(event)
 
   def handle_main_menu_input(self, event): # This function handles keyboard input when the game state is set to MAIN_MENU
     if event.key in (pygame.K_UP, pygame.K_w): # pressing the ↑ or W moves the selection up
@@ -67,7 +82,7 @@ class Game:
 
     elif event.key == pygame.K_RETURN: # if ENTER key is pressed and 3 heroes are selected move to encounter state
       if len(self.selected_heroes) == 3:
-        self.state = GameState.ENCOUNTER
+        self.state = GameState.DUNGEON_SELECTION
     
     elif event.key == pygame.K_ESCAPE: # if ESC is pressed return to main menu and clear hero selection
       self.state = GameState.MAIN_MENU
@@ -75,7 +90,24 @@ class Game:
 
     self.hero_selection %= len(self.available_heroes)
 
-          
+  def handle_dungeon_selection_input(self, event):
+    if event.key in (pygame.K_UP, pygame.K_w):
+      self.dungeon_selection -= 1
+
+    elif event.key in (pygame.K_DOWN, pygame.K_s):
+      self.dungeon_selection += 1
+
+    elif event.key == pygame.K_SPACE:
+      self.selected_dungeon = (self.available_dungeons[self.dungeon_selection])
+
+    elif event.key == pygame.K_RETURN:
+      if self.selected_dungeon is not None:
+        self.state = GameState.DUNGEON
+
+    elif event.key == pygame.K_ESCAPE:
+      self.state = GameState.TEAM_SELECTION
+
+    self.dungeon_selection %= len(self.available_dungeons)
 
 
   def update(self):
@@ -88,6 +120,14 @@ class Game:
       self.draw_main_menu()               # will draw the screen based on state
     elif self.state == GameState.TEAM_SELECTION:
       self.draw_team_selection()
+    elif self.state == GameState.DUNGEON_SELECTION:
+      self.draw_dungeon_selection()
+    elif self.state == GameState.DUNGEON:
+      self.draw_dungeon()
+    elif self.state == GameState.ENCOUNTER:
+      self.draw_encounter()
+    elif self.state == GameState.COMBAT:
+      self.draw_combat()
 
 
   def draw_main_menu(self):
@@ -156,6 +196,89 @@ class Game:
 
     instruction_rect = instruction_text.get_rect(center=(500, 630))
     self.screen.blit(instruction_text, instruction_rect)
+
+  def draw_dungeon_selection(self):
+    title_font = pygame.font.Font(None, 64)
+    dungeon_font = pygame.font.Font(None, 36)
+    info_font = pygame.font.Font(None, 28)
+
+    title = title_font.render("Select Your Dungeon", True, (255, 255, 255))
+
+    title_rect = title.get_rect(center=(500, 80))
+    self.screen.blit(title, title_rect)
+
+    for index, dungeon in enumerate(self.available_dungeons):
+      if index == self.dungeon_selection:
+        dungeon_color = (255, 215, 0)
+
+      elif dungeon == self.selected_dungeon:
+        dungeon_color = (0, 255, 0)
+
+      else:
+        dungeon_color = (255, 255, 255)
+
+      dungeon_text = dungeon_font.render(
+        f"{index + 1}. {dungeon}", True, dungeon_color
+      )
+
+      dungeon_rect = dungeon_text.get_rect(
+        center=(500, 220 + index * 80)
+      )
+
+      self.screen.blit(dungeon_text, dungeon_rect)
+
+    if self.selected_dungeon is None:
+      selected_text = "Selected Dungeon: None"
+    else:
+      selected_text = f"Selected Dungeon: {self.selected_dungeon}"
+
+    selected_surface = info_font.render(
+      selected_text, True, (255, 255, 255)
+    )
+
+    selected_rect = selected_surface.get_rect(center=(500, 500))
+    self.screen.blit(selected_surface, selected_rect)
+
+    instruction_text = info_font.render(
+      "Arrow Keys or W and S: Move | Space: Select | Enter: Confirm",
+      True,
+      (180, 180, 180)
+    )
+
+    instruction_rect = instruction_text.get_rect(center=(500,620))
+    self.screen.blit(instruction_text,instruction_rect)
+
+  def draw_dungeon(self):
+    title_font = pygame.font.Font(None, 64)
+    text_font = pygame.font.Font(None, 36)
+
+    title = title_font.render(
+      "Dugeon",
+      True,
+      (255, 255, 255)
+    )
+
+    title_rect = title.get_rect(center=(500, 100))
+    self.screen.blit(title, title_rect)
+
+    dungeon_text = text_font.render(
+      f"Entering: {self.selected_dungeon}",
+      True,
+      (255, 215, 0)
+    )
+
+    instruction_text = text_font.render(
+      "Dungeon progression coming soon...",
+      True,
+      (180, 180, 180)
+    )
+
+    instruction_rect = instruction_text.get_rect(center=(500, 500))
+    self.screen.blit(instruction_text, instruction_rect)
+
+    dungeon_rect = dungeon_text.get_rect(center=(500, 300))
+    self.screen.blit(dungeon_text, dungeon_rect)
+
 
   def run(self):
     clock = pygame.time.Clock()
